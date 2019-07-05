b46a36ae5baa55a00504c67483c1a7fa74455a31
Clément Aubin <aubincleme@gmail.com>
2019-07-05 15:21:04 +0200

    Move applicative content in passboltapi package

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ requires = [
 
 setuptools.setup(
     name="passbolt-api",
-    version="0.0.2",
+    version="0.0.3",
     author="XWiki SAS Development Team",
     author_email="pypi@xwiki.com",
     description="Provides API bindings for the Passbolt API",
